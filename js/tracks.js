822f93e620a4642ed4197f5243d3ea37fb34c6df
NickVuci <03_replays_relay@icloud.com>
2025-06-04 08:12:35 -0400

    deleted redundant traccks file, improved current one

--- js/tracks.js
+++ js/tracks.js
@@ -1,28 +1,41 @@
 /**
  * Track data for the audio player
+ * Enhanced with additional metadata fields
  */
 
 window.tracksData = {
-    // Track information
+    // Track information with extended metadata
     tracks: [
         {
             id: "01",
             title: "Softer (16-EDO)",
             artist: "Nick Vuci",
-            tuning: "16-tone equal temperament",
             file: "music/NickVuci-20220211-16edo-Softer.mp3",
             year: "2022",
-            description: "A composition in 16 tone equal temperament"
+            genre: "Xenharmonic",
+            tuning: "16-tone equal temperament",
+            album: null,
+            duration: null, // Will be populated when loaded
+            description: "A softer piece composed in 16 equal divisions of the octave"
         },
         {
             id: "02",
-            title: "Solo Piano Miniature 1",
-            artist: "Nick Vuci",
-            tuning: "24-tone equal temperament",
+            title: "Quarter-Tone Piano Miniature 1",
+            artist: "Nick Vuci", 
             file: "music/NV-20201021-24EDO-SoloPianoMiniature1.flac",
             year: "2020",
-            description: "A piano piece composed in 24 tone equal temperament"
+            genre: "Xenharmonic",
+            tuning: "24-tone equal temperament",
+            album: null,
+            duration: null, // Will be populated when loaded
+            description: "A solo piano miniature composed in 24 equal divisions of the octave"
         }
+        // Future tracks can be added here with additional metadata:
+        // tags: ["experimental", "ambient", "classical"],
+        // bpm: 120,
+        // key: "C major (in 24-EDO)",
+        // instruments: ["piano", "synthesizer"],
+        // mood: "contemplative",
     ],
     
     // Default settings
